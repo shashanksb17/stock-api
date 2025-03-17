60b0fcf9d7986c9c8cd0db0f3521a44b51ce04f2
shashanksb098 <shashank.basutkar@creddinv.in>
2025-03-17 10:41:26 +0530

    fixed

--- index.js
+++ index.js
@@ -6,20 +6,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(cors())
-// const allowedOrigins = ['http://localhost:3001','https://www.creddinv.in']; // Add more specific origins here if needed
 
-// // Configure CORS dynamically
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     // Allow requests with no origin (e.g., curl, Postman) or if origin is in allowed list
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, origin); // Reflect the requesting origin
-//     } else {
-//       // Allow all other origins with '*'
-//       callback(null, '*');
-//     }
-//   }
-// }));
 
 const stocks = [
     { ticker: 'BHARTIARTL.NS', label: 'Bharti Airtel Limited', allocation_percentage: 4.4 },
